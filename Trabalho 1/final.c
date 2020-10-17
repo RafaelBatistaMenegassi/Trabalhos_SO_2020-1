ffe49f4e547685c51f2087595d74471c0c666b60
Rafael Batista Menegassi <RafaelBatistaMenegassi@users.noreply.github.com>
2020-10-17 16:02:59 -0300

    Inserção de Tratamento de Erros no fork()
    Ademais, realização de menores correções e adição de comentários.

--- Trabalho 1/final.c
+++ Trabalho 1/final.c
@@ -5,14 +5,14 @@ Trabalho 1 de Sistemas Operacionais
 Integrantes: 
 - Jessica Oliveira - 13/0028983
 - Rafael Menegassi - 14/0159355
-- Victor Martorelli
+- Victor Martorelli - 16/0085012
 
 Especificacoes de sistema utilizadas para testes:
-- Ubuntu 18.04 (Máquina Virtual)
+- Ubuntu 18.04.4 (Máquina Virtual)
 - GCC versao 7.5.0 
 
 Compilação:
-gcc final.c
+gcc final.c -o final
 
 Execução:
 ./final [NUMERO_DE_PROCESSOS_DESEJADO] &
@@ -22,25 +22,45 @@ Execução:
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
 	int pid;
 
-	if(atoi(argv[1]) > 10) {
-		printf("\nArgumento invalido!\nForneca um argumento menor ou igual a 10.\n");
+	// Verificacao se o numero de processos passado via argumento e valido.
+	if(atoi(argv[1]) > 10) 
+	{
+		printf("\nArgumento invalido!\nPor gentileza, forneca um argumento menor ou igual a 10.\n");
 		exit(1);
 	}
 
-	for(int n = 1; n < atoi(argv[1]); n++) {
-		pid = fork();
-		if(pid == 0) {
-			printf("\nProcesso-filho numero %d\nppid %d -> pid %d\n", n, (int) getppid(), (int) getpid());
+	// Impressao de dados do processo-pai.
+	printf("\nProcesso-pai\npid %d\n", (int) getpid());
+
+	// Loop de tratamento de novos processos.
+	for(int n = 0; n < atoi(argv[1]); n++) 
+	{
+
+		int pid;
+		
+		// Caso nao seja possivel realizar o fork.
+		if ((pid = fork()) < 0)
+		{
+			printf("ERRO #%d - ", errno);
+			perror("Erro ao criar novo processo");
+			exit(1);
+		}
+		// Caso seja possivel realizar o fork.
+		else if(pid == 0) 
+		{
+			// Impressao de dados do processo-filho.
+			printf("\nProcesso-filho numero %d\nppid %d -> pid %d\n", n+1, (int) getppid(), (int) getpid());
 			sleep(30);
 			exit(0);
 		}
 	}
-
-	printf("\nProcesso-pai\npid %d\n", (int) getpid());
+	
 	sleep(30);
 
 	return 0;
