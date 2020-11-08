09d9107a4600304bcabb501f36e6710b24920466
Rafael Batista Menegassi <RafaelBatistaMenegassi@users.noreply.github.com>
2020-11-08 14:53:08 -0300

    Modificações no Trabalho 2

--- Trabalho 2/trabalho2.c
+++ Trabalho 2/trabalho2.c
@@ -20,6 +20,7 @@ Execução:
 */
 
 #define KEY 0x9355 // key <- quatro ultimos numeros da matricula do Rafael
+#define MAX_INTERACAO 5 // Interacoes desejadas de escrita e leitura de shm
 
 #include<unistd.h>
 #include<errno.h>
@@ -63,14 +64,14 @@ int v_sem()
 
 int main()
 {
-    int pid, idshm,  estado;
+    int pid, idshm, estado;
     struct shmid_ds buf;
     int *pshm;
 
     /* criacao de area de memoria compartilhada */
     if ((idshm = shmget(KEY, sizeof(int), IPC_CREAT|0x1ff)) < 0) 
     {
-        printf("Erro #%d\t", errno);
+        printf("\nErro #%d\t", errno);
         perror("Erro ao tentar criar area de memoria compartilhada - ");
         exit(1);
     }
@@ -78,76 +79,115 @@ int main()
     /* criacao de semaforo */
     if ((idsem = semget(KEY, 1, IPC_CREAT|0x1ff)) < 0)
     {
-        printf("Erro #%d\t", errno);
-        perror("Erro ao tentar criar semaforo - ");
+        printf("\nErro #%d\t", errno);
+        perror("\nErro ao tentar criar semaforo - ");
         exit(1);
     }
 
     /* criacao de processo filho */
     pid = fork();
 
-    for (int i = 1; i <= 3; i++) // Testar inicialmente com 3 e depois mudar para vinte
+    for (int i = 1; i < MAX_INTERACAO + 1; i++) // Testar inicialmente com 3 e depois mudar para vinte
     {
+
+        printf("\nentrei for -> pid %d", (int) getpid());
+
         if (pid == 0)
         {
             /* filho */
 
+            printf("\nentrei if filho");
+
+            int* shmaddr_filho;
+
             /* attach do filho em area compartilhada */
-            pshm = (int *) shmat(idshm, (char *)0, 0); // Ultimo argumento equivale a "O_RDWR"
+            pshm = (int *) shmat(idshm, shmaddr_filho, 0); // Ultimo argumento equivale a "O_RDWR"
 
             if (pshm == (int *)-1) 
             {
-                printf("Erro #%d\t", errno);
-                perror("Erro ao tentar attach - ");
+                printf("\nErro #%d\t", errno);
+                perror("\nErro ao tentar attach - ");
                 exit(1);
             }
 
             //printf("vou escrever\n");
 
-            p_sem();
+            //p_sem();
 
             *pshm = i;
 
-            printf("processo<%d> escreveu %d\n", (int) getpid(), *pshm /* ou i */);
+            printf("\nprocesso<%d> escreveu %d\n", (int) getpid(), *pshm /* ou i */);
 
             // TODO: inserir P(sem) (??) para garanir que a leitura sera feita apos a escrita
             
-            v_sem();
+            //v_sem();
+
+            /* detach */
+
+            if(shmdt(shmaddr_filho) < 0)
+            {
+                printf("\nErro ao tentar o detach!");
+                exit(1);
+            }
+
+            p_sem();
+
+	        if(i == MAX_INTERACAO)
+            {
+		        exit(0);
+            }
             
         }
 
         /* pai */
 
+        int* shmaddr_pai;
+
         /* attach do pai em area compartilhada */
-        pshm = (int *) shmat(idshm, (char *)0, 0); // Ultimo argumento equivale a "O_RDWR"
+        pshm = (int *) shmat(idshm, shmaddr_pai, 0); // Ultimo argumento equivale a "O_RDWR"
 
         if (*pshm == -1) 
         {
-            printf("Erro #%d\t", errno);
-            perror("Erro ao tentar attach - ");
+            printf("\nErro #%d\t", errno);
+            perror("\nErro ao tentar attach - ");
             exit(1);
         }
 
-        // TODO: inserir V(sem) (??) para garanir que a leitura sera feita apos a escrita
-
-        p_sem();
-
-        printf("processo<%d> leu %d\n", (int) getpid(), *pshm);
+        // TODO: inserir V(sem) (??) para garantir que a leitura sera feita apos a escrita
 
         v_sem();
+
+        //p_sem();
+
+        printf("\nprocesso<%d> leu %d\n", (int) getpid(), *pshm);
+
+        printf("\n\naaaaaaaaaaaaaaa");
+
+        //v_sem();
+
+        /* detach */
+
+        if(shmdt(shmaddr_pai) < 0)
+        {
+            printf("\nErro ao tentar o detach!");
+            exit(1);
+        }
+
     }
 
     /* finalizacao */
 
     // TODO: verificar se e preciso remover area de memoria compartilhada ao finalizar o programa
-    sleep(10);
+    //sleep(10);
 
     if (shmctl(idshm, IPC_RMID, 0) < 0)
     {
-        printf("Erro #%d\t", errno);
-        perror("Erro ao tentar remover area de memoria compartilhada - ");
+        printf("\nErro #%d\t", errno);
+        perror("\nErro ao tentar remover area de memoria compartilhada - ");
         exit(1);
-    }    
+    }   
+
+    //wait(&estado); 
 
     return 0;
    
