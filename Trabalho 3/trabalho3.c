1d21b58d85cb74b6369b4b3fae46055a6e571278
Jessica Oliveira <jessiicaasoliveira@gmail.com>
2020-11-18 15:02:50 -0800

    Adição do signal

--- Trabalho 3/trabalho3.c
+++ Trabalho 3/trabalho3.c
@@ -19,84 +19,111 @@ Execução:
 
 */
 
-#define MAX_MSGS 10
+#define MAX_MSGS 5
 #define MAX_SLEEP 4
 #define MIN_SLEEP 1
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-#include<unistd.h>
-#include<errno.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
 
-#include<sys/types.h>
-#include<sys/ipc.h>
-#include<sys/msg.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+void sig_handler(int signum){
+	printf("Inside handler function\n");
+}
 
 int main()
 {
-     int pid, idfila;
-     struct mensagem
-     {
-          long pid;
-          int msg;
-     };
+	int pid, idfila, segundos;
+	struct mensagem
+	{
+		long pid;
+		int msg;
+	};
 
-     struct mensagem mensagem_env, mensagem_rec; 
+	struct mensagem mensagem_env, mensagem_rec;
+	struct msqid_ds buf ; 
 
-     /* criacao da fila de mensagens */
-     if ((idfila = msgget(0x9355, IPC_CREAT|0x1B6)) < 0)
-     {
-          printf("erro na criacao da fila\n");
-          exit(1);
-     }
+	/* criacao da fila de mensagens */
+	if ((idfila = msgget(0x9355, IPC_CREAT|0x1B6)) < 0)
+	{
+		printf("erro na criacao da fila\n");
+		exit(1);
+	}
 
-     pid = fork();
+	pid = fork();
 
-     /* for que garante o envio de dez mensagens */  
-     for(int i = 1; i <= MAX_MSGS; i++)
-     {
-          if (pid == 0)
-          {
-               /* filho - P2 */
-               mensagem_env.pid = getpid();
-               mensagem_env.msg = i;
+	srand((unsigned)time(NULL));
 
-               /*
-               srand(time(0));
+	/* for que garante o envio de dez mensagens */  
+	for(int i = 1; i <= MAX_MSGS; i++)
+	{
+		/* filho - P2 (envia msg)*/
+		if (pid == 0)
+		{
+			segundos = 1+ (rand() % 4);
+			printf("%d\n", segundos);
+			sleep(segundos);
 
-               int sleepTime = (rand() % (MAX_SLEEP - MIN_SLEEP + 1)) + MIN_SLEEP;
+			mensagem_env.pid = getpid();
+			mensagem_env.msg = i;
 
-               printf("filho dormira por %d segundos\n", sleepTime);
+			msgsnd(idfila, &mensagem_env, sizeof(mensagem_env)-sizeof(long), 0);
 
-               sleep(sleepTime);
-               */
+			printf("mensagem <pid: %li, msg: %d> enviada!\n", mensagem_env.pid, mensagem_env.msg);
 
-               msgsnd(idfila, &mensagem_env, sizeof(mensagem_env)-sizeof(long), 0);
+			if (i == MAX_MSGS)
+			{
+				exit (0);	/* fim do filho */        
+			}
+		}
+		else
+		{
+			/* pai - P1 (recepção da msg) */
 
-               printf("mensagem <pid: %li, msg: %d> enviada!\n", mensagem_env.pid, mensagem_env.msg);
+			/* logica que garante que o pai espere no maximo dois segundos para receber */
 
-               if (i == MAX_MSGS)
-               {
-                    /* fim do filho */
-                    exit (0);               
-               }
-          }
-          else
-          {
-               /* pai - P1 */
+			signal(SIGALRM,sig_handler); // Register signal handler
 
-               /* logica que garante que o pai espere no maximo dois segundos para receber */
+			alarm(2);
 
-               msgrcv(idfila, &mensagem_rec, sizeof(mensagem_rec)-sizeof(long), 0, 0);
+			if (msgrcv(idfila, &mensagem_rec, sizeof(mensagem_rec)-sizeof(long), 0, 0) < 0)
+			{
+				if(errno == EINTR){
+        	printf("Mensagem não foi recebida em 2s.\n");
+				}
+				else
+				{
+					printf("Erro ao receber mensagem! Erro: %d", errno);
+				}
+			}
+			else
+			{
+				alarm(0);
+				printf("mensagem <pid: %li, msg: %d> recebida!\n", mensagem_rec.pid, mensagem_rec.msg);
+			}
+		}
+	}
 
-               printf("mensagem <pid: %li, msg: %d> recebida!\n", mensagem_rec.pid, mensagem_rec.msg);
+	if (msgctl(idfila, IPC_RMID, &buf) < 0)
+	{
+		printf("\nErro #%d\t", errno);
+    perror("\nErro ao tentar remover fila");
+    exit(1);
+  } 
+  else 
+  {
+    printf("Fila removida\n");
+  }
 
-          }
-     } 
-
-     //wait (&estado);
-     return 0;
+	return 0;
 }
-     
+	
